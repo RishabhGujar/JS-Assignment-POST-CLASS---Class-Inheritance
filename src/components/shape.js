177daf537a7627f1290ce60bb975b7680d61339a
RishabhGujar <87774343+RishabhGujar@users.noreply.github.com>
2021-08-07 12:13:01 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
 
 // export class using module.exports
+
+const Shape = require('./shape');
+class Circle extends Shape{
+    constructor(){
+        super();
+
+    }
+    calculateArea(){
+
+     return "hello"
+    }
+}
+exports.module = Circle;
